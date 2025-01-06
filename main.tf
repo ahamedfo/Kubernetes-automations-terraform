8b6ba6d83ac54ca4f5233da81e3dde70f3a3f42d
Ahamed Fofana <ahamedfofana12@gmail.com>
2025-01-06 11:45:02 -0500

    terraform and kuberbetes replacement for civo

---
+++ main.tf
@@ -0,0 +1,43 @@
+# kubernetes cluster
+
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "3.18.1"
+
+    name = "my-vpc"
+    cidr = "10.0.0.0/16"
+
+    azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "ahameds_cluster"
+  cluster_version = "1.27"
+
+#   cluster_endpoint_public_access = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = false
+  cluster_endpoint_private_access = true
+
+
+  eks_managed_node_groups = {
+    green = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = ["t3.micro"]
+    }
+  }
+}
